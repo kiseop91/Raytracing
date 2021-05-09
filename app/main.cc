4f61ed3642f710f83d4d8d28dcad257e2c502382
kiseop <kiseop91@naver.com>
2021-05-09 20:58:56 +0900

    [chapter 4] Rays, a Simple Camera, and Background

--- app/main.cc
+++ app/main.cc
@@ -1,21 +1,47 @@
-#include "shape.h"
+#include "color.h"
+#include "ray.h"
+#include "vec3.h"
+
 #include <iostream>
-#include <color.h>
-#include <vec3.h>
 
-int main()
-{
-  const int image_width = 256;
-  const int image_height = 256;
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5 * (unit_direction.y() + 1.0);
+    return color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
 
-  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+int main() {
 
-  for( int j = image_height- 1; j >=0; --j){
-    std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-    for (int i = 0; i< image_width; ++i){
-      color pixel_color(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
-      write_color(std::cout, pixel_color);
+    // Image
+    const float aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+
+    const float viewport_height = 2.0;
+    const float viewport_width = aspect_ratio * viewport_height;
+    const float focal_length = 1.0;
+
+    vec3 origin = point3(0, 0, 0);
+    vec3 horizontal = vec3(viewport_width, 0, 0);
+    vec3 vertical = vec3(0, viewport_height, 0);
+    vec3 lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+
+    // Render
+
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+    for (int j = image_height-1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            auto u = double(i) / (image_width-1);
+            auto v = double(j) / (image_height-1);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            color pixel_color = ray_color(r);
+            write_color(std::cout, pixel_color);
+        }
     }
-  }
-  std::cerr << "\nDone.\n";
+
+    std::cerr << "\nDone.\n";
 }
